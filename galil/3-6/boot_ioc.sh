03c3ea85ec9ceb39bd4835867020e909f8f08782
bleaktwig <bruno.benkel@gmail.com>
2022-08-09 09:17:24 -0400

    Updated scritps

---
+++ galil/3-6/boot_ioc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# NOTE. To exit the epics shell, use `ctrl + X`. If you hit `ctrl + C` the
+#       shell will stall! At this point the only way to kill it is to hit it
+#       IGKILL (It doesn't respond to SIGTERM). The killall.sh command is
+#       conveniently left on this directory to murder the shell in this
+#       scenario.
+cd "iocBoot/iocGalilTest/"
+../../bin/linux-arm/GalilTest st.cmd
+cd - > /dev/null
--- galil/3-6/kill_screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-killall --signal SIGKILL GalilTest
