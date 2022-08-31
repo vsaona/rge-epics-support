224afbbdbc85bed2510b3f8e629c6f250dd93b36
bleaktwig <bruno.benkel@gmail.com>
2022-08-31 17:07:18 +0100

    Typo

--- galil/3-6/boot_ioc.sh
+++ galil/3-6/boot_ioc.sh
@@ -2,7 +2,7 @@
 
 # NOTE. To exit the epics shell, use `ctrl + X`. If you hit `ctrl + C` the
 #       shell will stall! At this point the only way to kill it is to hit it
-#       IGKILL (It doesn't respond to SIGTERM). The killall.sh command is
+#       SIGKILL (It doesn't respond to SIGTERM). The killall.sh command is
 #       conveniently left on this directory to murder the shell in this
 #       scenario.
 cd "iocBoot/iocGalilTest/"
